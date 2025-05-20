9011371e01020b069827dc73b123422ded13af2a
cartaya.liz <cartayaliz2006@gmail.com>
2025-05-20 16:59:54 -0400

    Creating the visual part

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,26 +29,42 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(369, 246);
+            this.button1.Location = new System.Drawing.Point(722, 12);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
+            this.button1.Text = "Go";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(500, 500);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1218, 541);
+            this.ClientSize = new System.Drawing.Size(809, 662);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -56,6 +72,7 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -4,22 +4,99 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace walleproyect
 {
     public partial class Form1 : Form
     {
+        Random rand = new Random();
+        Dictionary<char, Color> mappedChars = new Dictionary<char, Color>();
+        Color[,] colors;
+        int n = 50;
+        int x = 5; int y = 6;
         public Form1()
         {
+            colors = new Color[n, n];
+
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    colors[i, j] = Color.White;
+
             InitializeComponent();
+            pictureBox1.Width = 500;
+            pictureBox1.Height = 500;
+
+            //
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
+          // Con cada click pinta de un color aleatorio una casilla aleatoria
+            Color color = Color.FromArgb(
+                rand.Next(256), // Rojo
+                rand.Next(256), // Verde
+                rand.Next(256)  // Azul
+            );
+            int i = rand.Next(n);
+            int j = rand.Next(n);
+            colors[i, j] = color;
+            pictureBox1.Refresh();
+        }
 
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            Graphics g = e.Graphics;
+            
+            int width = pictureBox1.Width;
+            int height = pictureBox1.Height;
+            Console.WriteLine(width);
+            Console.WriteLine(height);
+
+
+            int widthCell = width / n;   
+            int heightCell = height / n;
+
+            Pen p = new Pen(Color.Black);    // Para pintar bordes en negro
+           
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    float xx = j * widthCell;
+                    float yy = i * heightCell;
+                    if(i == x && j == y)
+                    {
+                        //Pintando ubicacion de Walle
+                        g.FillRectangle(Brushes.Gray, xx, yy, widthCell, heightCell);
+                        g.DrawEllipse(new Pen(Brushes.Magenta, 2), xx, yy, widthCell, heightCell);
+                        Font fuente = new Font("Arial", 15, FontStyle.Bold);
+                        Brush pincel = Brushes.DarkViolet;
+                        g.DrawString("W", fuente, pincel, xx + widthCell/4, yy + heightCell / 4);
+
+                    }
+                    else
+                    {
+                        SolidBrush brush = new SolidBrush(colors[i, j]);
+                        g.FillRectangle(brush, xx, yy, widthCell, heightCell);
+                    }
+                }
+            }
+            
+            for (int i = 0; i <= n; i++)
+            {
+                g.DrawLine(p, i * widthCell, 0, i * widthCell, height); //Dibujando lineas verticales
+                g.DrawLine(p, 0, i * heightCell, width, i * heightCell);// Dibujando lineas horizontales
+            }
         }
     }
 }
